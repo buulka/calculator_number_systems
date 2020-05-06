8685b090b50fb0510ecc6efc793840cdcd614d90
Ekaterina Ivanova <katenika158@gmail.com>
2020-05-07 00:31:49 +0300

    Translate integer numbers from any to any number system

--- Проект_политех/Program.vb
+++ Проект_политех/Program.vb
@@ -3,24 +3,29 @@ Module Program
     Sub Main()
 
         Dim num As String
-        Dim sum As Integer
         Dim osn As Integer
-        Dim abc As String = "0123456789ABCDEFGHIJKLMNOPQRSTWVXYZ"
         Dim osn2 As Integer
-        Console.Write("Enter the base of the number system from which you want to translate the number: ")
+
+        Console.Write("Base of the original number system: ")
         osn = Console.ReadLine()
+
+        Console.Write("Base of the result number system: ")
+        osn2 = Console.ReadLine()
 
         Console.Write("Number: ")
         num = Console.ReadLine()
 
-        Console.Write("В какую:")
-        osn2 = Console.ReadLine()
-
-        Console.WriteLine("В десятичной: " & NumberToDecimal(num, osn, abc))
-
+        Console.WriteLine("Result: " & Conversion(num, osn, osn2))
 
         Console.ReadKey()
     End Sub
+
+    Function Conversion(num As String, osn As Integer, osn2 As Integer) As String
+        Dim abc As String = "0123456789ABCDEFGHIJKLMNOPQRSTWVXYZ"
+        Dim decimal_number = NumberToDecimal(num, osn, abc)
+
+        Return NumberFromDecimal(decimal_number, osn2, abc)
+    End Function
     Function NumberToDecimal(num As String, osn As Integer, abc As String) As Integer
         Dim sum As Integer = 0
         Dim array(Len(num) - 1) As Integer
@@ -36,5 +41,16 @@ Module Program
         Return sum
     End Function
 
+    Function NumberFromDecimal(decimal_number As Integer, osn2 As Integer, abc As String) As String
+        Dim num2 As String = ""
+
+        Do
+            num2 += abc(decimal_number Mod osn2)
+            decimal_number \= osn2
+        Loop Until osn2 > decimal_number
+        num2 += abc(decimal_number)
+
+        Return StrReverse(num2)
+    End Function
 End Module
 
